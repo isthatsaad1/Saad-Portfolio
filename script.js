b418e2ce510ecad97dfe66db120dd9821035b8bd
isthatsaad1 <mr.rajpoooot862@gmail.com>
2024-11-03 11:02:58 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+
+        if (top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
+        }
+        // if want to use animation that repeats on scroll use this
+        else {
+            sec.classList.remove('show-animate');
+        }
+    });
+}
+
+
+// Send Mail Function
+function SendMail() {
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        from_email : document.getElementById("email_id").value,
+        message : document.getElementById("message").value,
+        number : document.getElementById("phoneNumber").value
+
+    }
+    emailjs.send("service_6dn98cc", "template_zxhys42", params).then(function (res) {
+        alert("Success!" + res.status);
+    })
+}
